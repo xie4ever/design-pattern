56501494bf57b81856fadeaf592bc22d30e33bd3
xie4ever <xie4ever@qq.com>
2021-03-01 21:51:34 +0800

    feat(project): init project

--- observer/badexample/example1/main.go
+++ observer/badexample/example1/main.go
@@ -1,1 +1,16 @@
-package example1
+package main
+
+import (
+	"time"
+
+	"design-pattern/observer/badexample/example1/article"
+)
+
+func main() {
+	a := article.NewArticle("title", "content")
+	a.Add()
+	time.Sleep(time.Second)
+	a.Modify()
+	time.Sleep(time.Second)
+	a.Delete()
+}
